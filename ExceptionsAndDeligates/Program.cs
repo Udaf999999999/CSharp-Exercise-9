6d59fa332e613f94b097bdb40f9150ddcba54fc5
Udaf999999999 <udaf999999999@yandex.ru>
2025-01-03 00:08:42 +0700

    Add deligates

--- ExceptionsAndDeligates/Program.cs
+++ ExceptionsAndDeligates/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace ExceptionsAndDeligates
+﻿namespace ExceptionsAndDeligates;
+internal class Program
 {
-    internal class Program
+    static int Division(int a, int b)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        return a / b;
+    }
+    public static void Minus(int a, int b) => Console.WriteLine(a - b); 
+    public static void Sum(int a , int b) => Console.WriteLine(b + a);
+    public delegate void MinusDeilgate(int a, int b);
+    public static int AddNumbers(int param1, int param2) => param1 + param2;
+    static void Main(string[] args)
+    {
+        MinusDeilgate minusDeilgate = Minus;
+        minusDeilgate += Sum;
+        minusDeilgate -= Minus;
+        minusDeilgate(6, 5);
+        Func<int, int, int> Addition = AddNumbers;
+        //try
+        //{
+        //    throw new RankException();
+        //}
+        //catch (Exception ex) when (ex is ArgumentOutOfRangeException)
+        //{
+        //    Console.WriteLine(ex.Message);
+        //}
+        //catch (RankException ex)
+        //{ 
+        //    Console.WriteLine(ex.GetType()); 
+        //}
     }
 }
