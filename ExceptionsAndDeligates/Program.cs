2165a55abf1e357b6a242596694d8229a1cfc446
Udaf999999999 <udaf999999999@yandex.ru>
2025-01-02 21:06:15 +0700

    Add solution

---
+++ ExceptionsAndDeligates/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ExceptionsAndDeligates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
