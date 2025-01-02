aa3617ae06cd982893c54df66869b3e44a1e86ef
Udaf999999999 <udaf999999999@yandex.ru>
2025-01-03 00:37:10 +0700

    Add Func, Action and Predicate deligates

--- ExceptionsAndDeligates/Program.cs
+++ ExceptionsAndDeligates/Program.cs
@@ -9,13 +9,19 @@ internal class Program
     public static void Sum(int a , int b) => Console.WriteLine(b + a);
     public delegate void MinusDeilgate(int a, int b);
     public static int AddNumbers(int param1, int param2) => param1 + param2;
+    public static void Display(string message) => Console.WriteLine(message);
+    public static bool IsApple(string modelName) => modelName == "IPhone X"? true: false;
     static void Main(string[] args)
     {
         MinusDeilgate minusDeilgate = Minus;
         minusDeilgate += Sum;
         minusDeilgate -= Minus;
         minusDeilgate(6, 5);
+
         Func<int, int, int> Addition = AddNumbers;
+        Action<string> action = new Action<string>(Display);
+        Predicate<string> predicate = IsApple;
+
         //try
         //{
         //    throw new RankException();
